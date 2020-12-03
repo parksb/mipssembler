6ede9139b3edc8b93f701731af92702877e4c65b
parksb <parkgds@gmail.com>
2020-12-03 19:21:31 +0900

    feat: Disassemble instructions

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,14 @@ mod constants;
 mod models;
 mod utils;
 
-use crate::constants::WORD;
-use crate::models::{Datum, Label, Section};
+use crate::constants::{INSTRUCTION_TABLES, WORD};
+use crate::models::{Argument, ArgumentType, Datum, Label, Section, Text};
 use crate::utils::read_lines;
 
 fn main() {
     let mut data: Vec<Datum> = vec![];
     let mut labels: Vec<Label> = vec![];
+    let mut texts: Vec<Text> = vec![];
 
     let args: Vec<String> = env::args().collect();
     let input_filepath = &args[1];
@@ -20,9 +21,11 @@ fn main() {
 
     let (data_section_size, text_section_size) =
         set_initial_values(&mut data, &mut labels, &mut input_file);
+    disassemble_instructions(&mut data, &mut labels, &mut texts, &mut input_file);
 
     println!("DATA: {:?}", data);
     println!("LABELS: {:?}", labels);
+    println!("TEXTS: {:?}", texts);
 }
 
 fn set_initial_values(
@@ -44,18 +47,143 @@ fn set_initial_values(
                 data_section_size += 1;
                 resolve_data(&line, current_address, &mut data);
                 current_address += WORD;
-            },
+            }
             Section::TEXT => {
                 text_section_size += 1;
                 if !resolve_labels(&line, current_address, &mut labels) {
                     current_address += WORD;
                 }
-            },
+            }
             Section::NONE => (),
         }
     }
 
     (data_section_size * WORD, text_section_size * WORD)
+}
+
+fn disassemble_instructions(
+    mut data: &mut Vec<Datum>,
+    mut labels: &mut Vec<Label>,
+    mut texts: &mut Vec<Text>,
+    mut input_file: &mut File,
+) {
+    let mut current_address = 0x10000000 - WORD;
+    let mut current_section = Section::NONE;
+    let mut text_section_size = 0;
+
+    for line in read_lines(&mut input_file, 0) {
+        current_section = resolve_section(&line).unwrap_or(current_section);
+
+        match current_section {
+            Section::TEXT => {
+                text_section_size += 1;
+                if !resolve_labels(&line, current_address, &mut labels) {
+                    if text_section_size > 1 {
+                        append_instructions_to_text(
+                            &line,
+                            current_address,
+                            &data,
+                            &labels,
+                            &mut texts,
+                        );
+                    }
+                    current_address += WORD;
+                }
+            }
+            _ => (),
+        }
+    }
+}
+
+fn append_instructions_to_text(
+    text: &str,
+    current_address: i32,
+    data: &Vec<Datum>,
+    labels: &Vec<Label>,
+    mut texts: &mut Vec<Text>,
+) {
+    if let [name, arguments] = text.trim_start().split('\t').collect::<Vec<&str>>()[..] {
+        let instruction_table = INSTRUCTION_TABLES
+            .iter()
+            .find(|table| table.compare_name(name))
+            .unwrap();
+
+        let mut new_text = instruction_table.to_text(0, current_address, vec![], false);
+
+        let argument_texts = arguments
+            .split(",")
+            .map(|arg| arg.trim())
+            .collect::<Vec<&str>>();
+
+        let mut stack_arguments: Vec<i32> = vec![];
+        let mut argument_values = argument_texts
+            .iter()
+            .filter_map(|argument_text| {
+                let argument_type = resolve_argument_type(argument_text);
+
+                match argument_type {
+                    ArgumentType::NUMBER => Some(convert_string_to_int(argument_text)),
+                    ArgumentType::REGISTER => Some(convert_string_to_int(
+                        &argument_text[1..argument_text.len()],
+                    )),
+                    ArgumentType::LABEL => {
+                        if let Some(datum) =
+                            data.iter().find(|datum| datum.compare_name(argument_text))
+                        {
+                            new_text.set_is_label(false);
+                            Some(datum.get_address())
+                        } else {
+                            if let Some(label) = labels
+                                .iter()
+                                .find(|label| label.compare_name(argument_text))
+                            {
+                                new_text.set_is_label(true);
+                                Some(label.get_address())
+                            } else {
+                                panic!("Failed to resolve argument value");
+                            }
+                        }
+                    }
+                    ArgumentType::STACK => {
+                        if let [offset, base] = argument_text.split('(').collect::<Vec<&str>>()[..]
+                        {
+                            let offset = convert_string_to_int(offset);
+                            let base = convert_string_to_int(&base[1..(base.len() - 1)]);
+
+                            stack_arguments = vec![offset, base];
+                            None
+                        } else {
+                            panic!("Failed to resolve argument value");
+                        }
+                    }
+                }
+            })
+            .collect::<Vec<i32>>();
+
+        if stack_arguments.len() > 0 {
+            argument_values.append(&mut stack_arguments);
+        }
+
+        new_text.set_arguments(argument_values);
+
+        texts.push(new_text);
+    }
+}
+
+fn resolve_argument_type(text: &str) -> ArgumentType {
+    let arguments = [
+        (Regex::new(r"^\$\d*").unwrap(), ArgumentType::REGISTER),
+        (Regex::new(r"^[a-z]\w*").unwrap(), ArgumentType::LABEL),
+        (Regex::new(r"^-?\d+\(\$\d*\)").unwrap(), ArgumentType::STACK),
+        (Regex::new(r"^(0x)?\d*").unwrap(), ArgumentType::NUMBER),
+    ];
+
+    arguments
+        .iter()
+        .find(|arg| arg.0.is_match(text))
+        .expect("Failed to resolve argument.")
+        .clone()
+        .1
 }
 
 fn resolve_section(text: &str) -> Result<Section, Section> {
@@ -70,7 +198,7 @@ fn resolve_data(text: &str, current_address: i32, data: &mut Vec<Datum>) -> bool
     let is_data = true;
     if let [name, meta] = text.split(":\t").collect::<Vec<&str>>()[..] {
         if let [_, value] = meta.split('\t').collect::<Vec<&str>>()[..] {
-            let parsed_value = i32::from_str_radix(value.trim_start_matches("0x"), 16).unwrap();
+            let parsed_value = convert_string_to_int(value);
             data.push(Datum::new(name, parsed_value, current_address));
             return is_data;
         }
@@ -89,5 +217,13 @@ fn resolve_labels(text: &str, current_address: i32, labels: &mut Vec<Label>) -> 
         is_label
     } else {
         !is_label
+    }
+}
+
+fn convert_string_to_int(text: &str) -> i32 {
+    if text.chars().nth(0).unwrap() == '-' {
+        i32::from_str_radix(text[1..text.len()].trim_start_matches("0x"), 16).unwrap() * -1
+    } else {
+        i32::from_str_radix(text.trim_start_matches("0x"), 16).unwrap()
     }
 }
--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 pub struct InstructionTable {
     name: &'static str,
     pointer: i32,
@@ -25,6 +27,22 @@ impl InstructionTable {
             is_occupied,
         }
     }
+
+    pub fn compare_name(&self, name: &str) -> bool {
+        self.name == name
+    }
+
+    pub fn to_text(&self, next: i32, address: i32, arguments: Vec<i32>, is_label: bool) -> Text {
+        Text {
+            next,
+            instruction: self.name.to_string(),
+            opcode: self.opcode,
+            funct: self.funct,
+            address,
+            arguments,
+            is_label,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -42,16 +60,47 @@ impl Datum {
             address,
         }
     }
+
+    pub fn compare_name(&self, name: &str) -> bool {
+        self.name == name
+    }
+
+    pub fn get_address(&self) -> i32 {
+        self.address
+    }
 }
 
+#[derive(Debug)]
 pub struct Text {
     next: i32,
     instruction: String,
     opcode: i32,
     funct: i32,
     address: i32,
-    arguments: [i32; 4],
+    arguments: Vec<i32>,
     is_label: bool,
+}
+
+impl Text {
+    pub fn new(instruction: &str, opcode: i32, funct: i32, address: i32) -> Self {
+        Self {
+            next: 0,
+            instruction: instruction.to_string(),
+            opcode,
+            funct,
+            address,
+            arguments: vec![],
+            is_label: false,
+        }
+    }
+
+    pub fn set_is_label(&mut self, is_label: bool) {
+        self.is_label = is_label;
+    }
+
+    pub fn set_arguments(&mut self, arguments: Vec<i32>) {
+        self.arguments = arguments;
+    }
 }
 
 #[derive(Debug)]
@@ -70,8 +119,12 @@ impl Label {
         }
     }
 
-    pub fn set_address_to_negative_one(&mut self) {
-        self.address = -1;
+    pub fn compare_name(&self, name: &str) -> bool {
+        self.name == name.to_string()
+    }
+
+    pub fn get_address(&self) -> i32 {
+        self.address
     }
 }
 
@@ -80,4 +133,31 @@ pub enum Section {
     NONE,
     DATA,
     TEXT,
+}
+
+#[derive(Clone)]
+pub struct Argument {
+    pub regex: Regex,
+    pub data_type: ArgumentType,
+}
+
+impl Argument {
+    pub fn new(regex: Regex, data_type: ArgumentType) -> Self {
+        Argument { regex, data_type }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum ArgumentType {
+    NUMBER,
+    REGISTER,
+    LABEL,
+    STACK,
+}
+
+#[derive(Debug)]
+pub enum InstructionFormat {
+    R,
+    I,
+    J,
 }
